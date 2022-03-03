dea13881240f852dc97f28c5a1f1da236f4d7844
Scarlet <scarletemmat@gmail.com>
2022-03-02 22:54:55 -0500

    Done with new rooms and go command.

--- commands.py
+++ commands.py
@@ -10,6 +10,9 @@ usage: look
 Allows the player to 'look' around the room. This function should print a description
 of the room to the player's terminal.
 """
+from hashlib import new
+
+
 def look_command(room):
     print(room.get_description())
     return None
@@ -23,8 +26,17 @@ Allows the player to go to another room. This function should move the player in
 they indicated. If there is no room in that direction, do not move the player and print to 
 the user that the player has not moved. If there is a room, move the player to that room and 
 print a message indicating the player has moved.
+
+This function should return the ROOM OBJECT of the new room.
 """
-def go_command(room_map, direction):
+def go_command(room_map, current_room, direction):
     # TODO: Implement Go Command
     print(f"I got told to go {direction}.")
-    return None 
+
+    new_room = room_map[current_room.get_name()][direction] 
+
+    if new_room == None:
+        print(f"This is a dead end, where are you going?")
+        return current_room
+    else:
+        return new_room 
--- game.py
+++ game.py
@@ -27,23 +27,35 @@ if __name__ == "__main__":
 	print_intro()
 
 	# Initialize Game Map
-	start_room = Room("room_start", "A Lab Room")
+	start_room = Room("room_start", "Your eyes slowly open to find yourself in a strange room. It is in what appears to be an old house. The room has white paint chipping off the walls with blacked out windows and two solitary pieces of furniture, a desk and a bed which you are currently laying on. You stand from the bed and start to walk towards the desk. Once you arrive in front of it, you notice the ratty piece of paper with blotted out ink spelling out six words 'Welcome to The Palace, Good Luck' Good Luck? What's that supposed to mean? You wonder as you make your way to the door, unaware of the multiple pairs of eyes watching you from the walls of The Palace.")
 
 	# Set Starting Room
 	CURRENT_ROOM = start_room
 
 	# TODO: Create Additional Rooms
+	room_op_1 = Room("The OR", "You turn to open the door to the west and arrive in a room splattered in blood. It appears to have been an old operating room; it had several different sized knives, many lights hanging from the ceiling, and one lone table right in the middle of it. You walk towards the table to find another piece of paper written in blotted ink. On it are, once again, six words, 'Strike One, three strikes, you're out.'")
+
+	room_op_2 = Room("Annabell", "This room contains a terrifying number of hairless dolls.")
+
+	room_op_3 = Room("The Carnival", "This room has an insane number of knives and swords stuck in the walls, like a demented carnival trick.")
+
+	room_op_4 = Room("Surprise", "Looking inside this room, it has a number of cardboard boxes each containing a different object.")
 
 	# Add Initial Room to Map
 	GAME_MAP[start_room.get_name()] = {
-		"object": start_room,
-		"north": None,
-		"south": None, 
-		"east": None,
-		"west": None
+		"north": room_op_3,
+		"south": room_op_2, 
+		"east": room_op_4,
+		"west": room_op_1
 	}
 
 	# TODO: Add Rooms to the Game Map
+	GAME_MAP[room_op_1.get_name()] = {
+		"north": None,
+		"south": None, 
+		"east": start_room,
+		"west": None
+	}
 
 	while(not GAMEOVER):
 		# Get Command from the player
@@ -54,7 +66,7 @@ if __name__ == "__main__":
 			look_command(CURRENT_ROOM)
 		elif command == 'go':
 			direction = command_list[1]
-			go_command(GAME_MAP, direction)
+			CURRENT_ROOM = go_command(GAME_MAP, CURRENT_ROOM, direction)
 		else:
 			print(f"{command} is not a valid command. Please try a valid command.\n")
 			continue
